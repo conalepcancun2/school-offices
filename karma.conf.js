a9abc28163d67bec4e201a5849b07c31353137b4
conalepcancun2 <niger.p@conalepqroo.edu.mx>
2021-02-17 12:11:23 -0500

    Changing into new name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/conalep-staff'),
+      dir: require('path').join(__dirname, './coverage/school-offices'),
       subdir: '.',
       reporters: [
         { type: 'html' },
